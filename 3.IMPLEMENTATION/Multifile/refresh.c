32c2927046255a0e4836ac876acd3069bc5fb79e
ShivaliGokhale <86190217+ShivaliGokhale@users.noreply.github.com>
2021-07-13 10:57:32 +0530

    Add files via upload
    add

---
+++ 3.IMPLEMENTATION/Multifile/refresh.c
@@ -0,0 +1,16 @@
+#include"cafe.h"
+/**
+ * @brief refreshOrderlist declared
+ * 
+ * @return int 
+ */
+
+int refreshOrderlist(){
+    int i=0;
+    char order[10][20]={""};
+    while(strlen(order[i])!=0){
+        strcpy(order[i],"xyz");
+        i++;
+    }
+
+}
