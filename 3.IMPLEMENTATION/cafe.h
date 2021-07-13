a4a8883b96c8b580ea12ffb9058e836461d3d777
ShivaliGokhale <86190217+ShivaliGokhale@users.noreply.github.com>
2021-07-13 08:37:42 +0530

    Add files via upload
    add

---
+++ 3.IMPLEMENTATION/cafe.h
@@ -0,0 +1,7 @@
+#ifndef __CAFE_H
+#define __CAFE_H
+
+int displayOrder();
+int refreshOrder();
+
+#endif
---
+++ 3.IMPLEMENTATION/refresh.c
@@ -0,0 +1,19 @@
+#include"cafe.h"
+/**
+ * @brief refreshOrderlist declared
+ * 
+ * @return int 
+ */
+
+int refreshOrderlist(){
+    int i=0 ,j;
+
+    char order[10][20]={"0"};
+    int totalPrice=0;
+    while(strlen(order[i])!=0){
+        strcpy(order[i],"xyz");
+        i++;
+    }
+    j = 0;
+    totalPrice = 0;
+}
