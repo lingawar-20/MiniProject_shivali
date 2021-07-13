d7db30a8605f8a101cf7843c6232fc6b45aa2535
ShivaliGokhale <86190217+ShivaliGokhale@users.noreply.github.com>
2021-07-13 10:35:55 +0530

    Add files via upload
    add

---
+++ 3.IMPLEMENTATION/Multifile/display.c
@@ -0,0 +1,20 @@
+#include "cafe.h"
+/**
+ * @brief displayOrder declared
+ * 
+ * @return int 
+ */
+
+int displayOrder(){
+    int i=0;
+    int totalPrice=0;
+    char order[10][20]={""};
+    printf("\n     Your Final Order     \n");
+    while(strlen(order[i])!=0){
+        printf("item = %s, quantity = %s\n",order[i],order[i+1]);
+        i = i+2;
+    }
+    printf("Total Price = %d",totalPrice);
+    printf("\n\n\nThank you for visiting our cafe");
+    
+}
